5b67e22dabc2ef27a7c3a2d92fbff6f91bde62d7
Alaa Mahrous <alamhrws8@gmail.com>
2025-02-24 18:31:21 +0200

    init hive and generate notes model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,16 @@
-class NoteModel {
-  String category;
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  String noteTitle;
+  @HiveField(1)
   String noteText;
+  @HiveField(2)
   DateTime date;
 
-  NoteModel({required this.category, required this.noteText})
+  NoteModel({required this.noteTitle, required this.noteText})
     : date = DateTime.now();
 }
